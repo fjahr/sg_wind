422254b39e4c00acfae56861c96d015d7d0042c4
Fabian Jahr <fabian.jahr@whu.edu>
2017-04-27 15:43:31 +0200

    remove some unnecessary code

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -35,50 +35,6 @@ class ObservationsController < ApplicationController
     render status: 200, text: "success"
   end
 
-  # GET /observations/1/edit
-  def edit
-  end
-
-  # POST /observations
-  # POST /observations.json
-  def create
-    @observation = Observation.new(observation_params)
-
-    respond_to do |format|
-      if @observation.save
-        format.html { redirect_to @observation, notice: 'Observation was successfully created.' }
-        format.json { render :show, status: :created, location: @observation }
-      else
-        format.html { render :new }
-        format.json { render json: @observation.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /observations/1
-  # PATCH/PUT /observations/1.json
-  def update
-    respond_to do |format|
-      if @observation.update(observation_params)
-        format.html { redirect_to @observation, notice: 'Observation was successfully updated.' }
-        format.json { render :show, status: :ok, location: @observation }
-      else
-        format.html { render :edit }
-        format.json { render json: @observation.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /observations/1
-  # DELETE /observations/1.json
-  def destroy
-    @observation.destroy
-    respond_to do |format|
-      format.html { redirect_to observations_url, notice: 'Observation was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_observation
