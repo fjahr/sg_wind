4771b6cc3d412f06a80c622708aa9654bb84f8e5
Fabian Jahr <fabian.jahr@whu.edu>
2017-04-28 15:19:55 +0200

    another try with refreshed token

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -58,12 +58,7 @@ class ObservationsController < ApplicationController
     @observation.save
 
     Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
-      Bot.deliver({
-          recipient: {'id': '548314146'},
-                  message: {
-                      text: "new value recorded"
-                          }
-                }, access_token: ENV["ACCESS_TOKEN"])
+      Bot.deliver({recipient: {'id': '548314146'},message: {text: "new value recorded"}}, access_token: ENV["ACCESS_TOKEN"])
 
     render status: 200, text: "success"
   end
