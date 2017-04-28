c53c9b441e94c98eeecd541da24e53bbdab32585
Fabian Jahr <fabian.jahr@whu.edu>
2017-04-28 13:51:31 +0200

    add facebook messenger bot initializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :observations, only: [:index, :show, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  Rails.application.routes.draw do
+    mount Facebook::Messenger::Server, at: "bot"
+  end
+
   root to: "observations#index"
 end
